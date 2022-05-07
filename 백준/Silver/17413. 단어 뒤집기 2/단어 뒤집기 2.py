1049cb407038f70c8cfe81089643c20f964f12c4
hyunkyungju <93658689+hyunkyungju@users.noreply.github.com>
2022-05-07 09:08:10 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 136 ms, Memory: 116964 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -2,61 +2,16 @@ import sys
 
 input = sys.stdin.readline
 
-class Stack:
-  def __init__(self):
-    self.st = list()
+s = input().rstrip()
 
-  def push(self, x):
-    self.st.append(x)
+a = s.replace('>','<').split('<')
 
-  def pop(self):
-    return self.st.pop()
-
-  def top(self):
-    return self.st[-1]  
-
-  def size(self):
-    return len(self.st)
-
-  def print_all(self):
-    while self.size()!=0:
-      print(self.pop(),end='')
-  
-  def clear(self):
-    self.st = list()
-
-st = Stack()
-
-str = input().rstrip()
-tag_stage = False
-tag_start_index = 0
-for i in range (len(str)):
-  c = str[i]
-  if tag_stage: 
-    if c=='>':
-      while st.top()!='<':
-        st.pop()
-      st.pop()
-      if not st.size():
-        for id in range(tag_start_index, i+1):
-          print(str[id], end='')
-        tag_stage=False
-    else:
-      st.push(c)
-
+result = ""
+for i in range(len(a)):
+  if i%2: 
+    result += '<'+a[i]+'>'
   else:
-    if c=='<':
-      st.print_all()
-      st.clear()
-      tag_start_index = i
-      tag_stage = True
-      st.push(c)
-    else:
-      if c!=' ':
-        st.push(c)
-      else:
-        st.print_all()
-        st.clear()
-        print(' ',end='')
-st.print_all()
-st.clear()
+    c = a[i].split()
+    result+=' '.join([d[::-1] for d in c])
+
+print(result)
