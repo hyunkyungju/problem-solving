35d4b8059d3980fb25fb41d9bb774fbce11e92b4
hyunkyungju <93658689+hyunkyungju@users.noreply.github.com>
2022-07-25 21:51:59 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+def sol(n, d):
+  d[1] = 1
+  if n==1:
+    return
+  d[2] = 3
+  for i in range(3, n+1):
+    d[i] = (2*d[i-2] + d[i-1]) % 10007
+    
+n = int(input())
+d = [0]*(n+1)
+
+sol(n, d)
+print(d[n])
