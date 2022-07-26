ca31aae9f669aa86e03997d19774dfa1e718986c
hyunkyungju <93658689+hyunkyungju@users.noreply.github.com>
2022-07-27 01:02:28 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 116 ms, Memory: 114488 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -5,12 +5,26 @@ n = int(input())
 
 numbers = list(map(int, input().split()))
 maxes = [0, numbers[0]]
+roads = [0, maxes[1:]]
+
 for number in numbers:
   for cnt, max_ in enumerate(maxes):
     if max_>number:
       if maxes[cnt-1] < number:
         maxes[cnt] = number
+        roads[cnt][cnt-1] = number
+        roads[cnt] = maxes[1:cnt+1]  
       break
   if max(maxes)<number:
     maxes.append(number)
-print(maxes.index(max(maxes)))
+    tmp = roads[-1][:]
+    tmp.append(number)
+    roads.append(tmp)
+
+maxes.pop(0)
+print(len(maxes))
+# print(' '.join(map(str, roads[len(maxes)])))
+'''
+6
+1 3 5 7 2 3
+'''
