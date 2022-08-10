9896e8b687c8e09c4cff158f7af42b8383389991
hyunkyungju <93658689+hyunkyungju@users.noreply.github.com>
2022-08-10 21:30:55 +0900

    [Silver III] Title: 다음 순열, Time: 128 ms, Memory: 115328 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/다음 순열.py
@@ -0,0 +1,18 @@
+import sys
+import itertools
+
+input = sys.stdin.readline
+
+n = int(input())
+ips = list(map(int, input().split()))
+
+for i in range(n - 1, 0, -1):
+    if ips[i - 1] < ips[i]:
+        for j in range(n - 1, 0, -1):
+            if ips[i - 1] < ips[j]:
+                ips[i - 1], ips[j] = ips[j], ips[i - 1]
+                ips = ips[:i] + sorted(ips[i:])
+                print(*ips)
+                exit()
+
+print(-1)
