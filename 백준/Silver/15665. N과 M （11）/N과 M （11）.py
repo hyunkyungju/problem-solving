7cbe6ea5538e068827315c12af1b5db2929b8d18
hyunkyungju <93658689+hyunkyungju@users.noreply.github.com>
2022-08-10 19:23:55 +0900

    [Silver II] Title: N과 M (11), Time: 388 ms, Memory: 119264 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -0,0 +1,21 @@
+import sys
+import itertools
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+ips = sorted(list(map(int, input().split())))
+visited = [False] * n
+
+
+def bt(s):
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+    before = 0
+    for i, v in enumerate(ips):
+        if before != v:
+            before = v
+            bt(s + [v])
+
+bt([])
