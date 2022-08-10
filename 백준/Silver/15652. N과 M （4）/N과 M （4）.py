52b3163b9b9c11310f390096b2559767cdc6cb3d
hyunkyungju <93658689+hyunkyungju@users.noreply.github.com>
2022-08-10 16:08:22 +0900

    [Silver III] Title: N과 M (4), Time: 204 ms, Memory: 118492 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,18 @@
+import sys
+import itertools
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+
+def bt(s):
+    if len(s) == m + 1:
+        s.pop(0)
+        print(' '.join(map(str, s)))
+        return
+    for i in range(s[-1], n + 1):
+        bt(s + [i])
+
+
+bt([1])
