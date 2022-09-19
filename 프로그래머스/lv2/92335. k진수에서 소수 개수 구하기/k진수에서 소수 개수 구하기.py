cdb0f84fbf1084cf2b67d495512d7c40235a4d25
hyunkyungju <93658689+hyunkyungju@users.noreply.github.com>
2022-09-19 19:45:13 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -1,32 +1,22 @@
 def is_prime(num):
+    if num <= 1:
+        return False
     for i in range(2, int(num ** (1/2)) + 1):
         if num % i == 0:
             return False
     return True
 
 def solution(n, k):
-    lst = list()
-    knum = 0
-    i = 0
-    while n >= k:
-        knum += (n % k) * pow(10, i)
-        i += 1
+    word = ""
+    while n:
+        word = str(n % k) + word 
         n //= k
-    knum += n * pow(10, i)
-    print(knum) 
-    lst = str(knum).split('0')
-    lst = [int(v) for v in lst if v ]
+    print(word) 
+    lst = [int(v) for v in word.split('0') if v ]
     answer = 0
-    
-    if not lst:
-        return 0
-    max_ = max(lst)
-    
-    if not max_ or max_ < 2:
-        return 0
 
     for v in lst:
-        if v > 1 and is_prime(v):
+        if is_prime(v):
             answer += 1 
     
     return answer
