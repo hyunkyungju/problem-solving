6a146ec768c039ad7b1b8c46d2c4f20b4e259378
hyunkyungju <93658689+hyunkyungju@users.noreply.github.com>
2022-10-14 23:32:15 +0900

    [Platinum V] Title: 온풍기 안녕!, Time: 452 ms, Memory: 121376 KB -BaekjoonHub

--- 백준/Platinum/23289. 온풍기 안녕！/온풍기 안녕！.py
+++ 백준/Platinum/23289. 온풍기 안녕！/온풍기 안녕！.py
@@ -1,216 +1,151 @@
 from collections import deque
 
-def print_table(table):
-    for i in range(1, r+1):
-        for j in range(1, c+1):
-            print(table[i][j], end=' ')
-        print()
-    print("----")
+dr = [0, -1, 0, 1]
+dc = [1, 0, -1, 0]
+
+n, m, k = map(int, input().split())
+table = [[0]*(m+1) for _ in range(n+1)]
+check_list = []
+heaters = []
+blocks = [[[False]*4 for _ in range(m+1)] for _ in range(n+1)]
 
 
-def change(tmp_table, q, is_visited, nr, nc, power):
-    tmp_table[nr][nc] += power - 1
-    is_visited[nr][nc] = True
-    if power > 1:
-        q.append((nr, nc, power - 1))
+for i in range(n):
+    lst = list(map(int, input().split()))
+    for j, val in enumerate(lst):
+        if not val:
+            continue
+        elif val == 5:
+            check_list.append((i+1, j+1))
+        elif val == 1 or val == 4:
+            heaters.append((i+1, j+1, val-1))
+        elif val == 2:
+            heaters.append((i + 1, j + 1, val))
+        elif val == 3:
+            heaters.append((i + 1, j + 1, 1))
+
+w = int(input())
+for _ in range(w):
+    x, y, t = map(int, input().split())
+    if not t:
+        blocks[x][y][1] = True
+        blocks[x-1][y][3] = True
+    else:
+        blocks[x][y][0] = True
+        blocks[x][y+1][2] = True
 
 
-def in_range(vr, vc):
-    return 0 < vr < r+1 and 0 < vc < c+1
+def in_range(nr, nc):
+    return 1 <= nr < n+1 and 1 <= nc < m + 1
 
 
-def bfs_initialize(sr, sc, tmp_table):
-    is_visited = [[False]*(c+1) for _ in range(r + 1)]
-    q = deque()
-    q.append((sr, sc, 5))
-    is_visited[sr][sc] = True
-    tmp_table[sr][sc] += 5
-    return is_visited, q
+def put_v(q, is_visited, r, c, num):
+    q.append((r, c))
+    is_visited[r][c] = True
+    table[r][c] += num
 
 
-def left_bfs(sr, sc, tmp_table):
-    is_visited, q = bfs_initialize(sr, sc, tmp_table)
-    while q:
-        vr, vc, power = q.popleft()
-        nr, nc = vr - 1, vc - 1
-        if in_range(nr, nc) and not wall[vr][vc][0] and not wall[vr-1][vc-1][1] and not is_visited[nr][nc]:
-            change(tmp_table, q, is_visited, nr, nc, power)
-        nr += 1
-        if in_range(nr, nc) and not wall[vr][vc-1][1] and not is_visited[nr][nc]:
-            change(tmp_table, q, is_visited, nr, nc, power)
-        nr += 1
-        if in_range(nr, nc) and not wall[vr+1][vc][0] and not wall[vr+1][vc-1][1] and not is_visited[nr][nc]:
-            change(tmp_table, q, is_visited, nr, nc, power)
+def wind():
+    for hr, hc, hd in heaters:
+        q = deque()
+        is_visited = [[False]*(m+1) for _ in range(n+1)]
+        nr = hr+dr[hd]
+        nc = hc+dc[hd]
 
+        put_v(q, is_visited, nr, nc, 5)
 
+        for num in range(4, 0, -1):
+            len_q = len(q)
+            for _ in range(len_q):
+                vr, vc = q.popleft()
+                for idx in [1, 3]:
+                    nr = vr + dr[hd] + dr[(hd+idx)%4]
+                    nc = vc + dc[hd] + dc[(hd+idx)%4]
+                    if in_range(nr, nc) and not is_visited[nr][nc] and not blocks[vr][vc][(hd+idx)%4] and not blocks[nr][nc][(hd+2)%4]:
+                        put_v(q, is_visited, nr, nc, num)
 
-def right_bfs(sr, sc, tmp_table):
-    is_visited, q = bfs_initialize(sr, sc, tmp_table)
-    while q:
-        vr, vc, power = q.popleft()
-        nr, nc = vr - 1, vc + 1
-        if in_range(nr, nc) and not wall[vr][vc][0] and not wall[vr-1][vc][1] and not is_visited[nr][nc]:
-            change(tmp_table, q, is_visited, nr, nc, power)
-        nr += 1
-        if in_range(nr, nc) and not wall[vr][vc][1] and not is_visited[nr][nc]:
-            change(tmp_table, q, is_visited, nr, nc, power)
-        nr += 1
-        if in_range(nr, nc) and not wall[vr+1][vc][0] and not wall[vr+1][vc][1] and not is_visited[nr][nc]:
-            change(tmp_table, q, is_visited, nr, nc, power)
-
-
-def up_bfs(sr, sc, tmp_table):
-    is_visited, q = bfs_initialize(sr, sc, tmp_table)
-    while q:
-        vr, vc, power = q.popleft()
-        nr, nc = vr-1 , vc-1
-        if in_range(nr, nc) and not wall[vr][vc-1][1] and not wall[vr][vc-1][0] and not is_visited[nr][nc]:
-            change(tmp_table, q, is_visited, nr, nc, power)
-        nc += 1
-        if in_range(nr, nc) and not wall[vr][vc][0] and not is_visited[nr][nc]:
-            change(tmp_table, q, is_visited, nr, nc, power)
-        nc += 1
-        if in_range(nr, nc) and not wall[vr][vc][1] and not wall[vr][vc+1][0] and not is_visited[nr][nc]:
-            change(tmp_table, q, is_visited, nr, nc, power)
-
-
-
-def down_bfs(sr, sc, tmp_table):
-    is_visited, q = bfs_initialize(sr, sc, tmp_table)
-    while q:
-        vr, vc, power = q.popleft()
-        nr, nc = vr+1 , vc-1
-        if in_range(nr, nc) and not wall[vr][vc-1][1] and not wall[vr+1][vc-1][0] and not is_visited[nr][nc]:
-            change(tmp_table, q, is_visited, nr, nc, power)
-        nc += 1
-        if in_range(nr, nc) and not wall[vr+1][vc][0] and not is_visited[nr][nc]:
-            change(tmp_table, q, is_visited, nr, nc, power)
-        nc += 1
-        if in_range(nr, nc) and not wall[vr][vc][1] and not wall[vr+1][vc+1][0] and not is_visited[nr][nc]:
-            change(tmp_table, q, is_visited, nr, nc, power)
+                nr = vr + dr[hd]
+                nc = vc + dc[hd]
+                if in_range(nr, nc) and not is_visited[nr][nc] and not blocks[nr][nc][(hd + 2) % 4]:
+                    put_v(q, is_visited, nr, nc, num)
 
 
 
 
-def machine():
-    tmp_table = [[0]*(c+1) for _ in range(r+1)]
-    for mr, mc, md in machines:
-        if md == 1:
-            tr, tc = mr, mc+1
-            right_bfs(tr, tc, tmp_table)
-        elif md == 2:
-            tr, tc = mr, mc-1
-            left_bfs(tr, tc, tmp_table)
-        elif md == 3:
-            tr, tc = mr-1, mc
-            up_bfs(tr, tc, tmp_table)
-        elif md == 4:
-            tr, tc = mr+1, mc
-            down_bfs(tr, tc, tmp_table)
-    for i in range(1, r+1):
-        for j in range(1, c+1):
-            table[i][j] += tmp_table[i][j]
+chocolate = 0
 
-
-def control_diff(tmp_table, bigi, smalli, bigj, smallj):
-    if table[bigi][bigj] == table[smalli][smallj]:
+def change_tmp_table(tmp_table, sr, sc, ar, ac):
+    if table[sr][sc] == table[ar][ac]:
         return
-    diff = (table[bigi][bigj] - table[smalli][smallj]) // 4
-    tmp_table[bigi][bigj] -= diff
-    tmp_table[smalli][smallj] += diff
+    diff = (table[sr][sc] - table[ar][ac]) // 4
+    tmp_table[sr][sc] -= diff
+    tmp_table[ar][ac] += diff
 
 
-def control():
-    tmp_table = [[0]*(c+1) for _ in range(r+1)]
-    for i in range(1, r+1):
-        for j in range(1, c):
-            if not wall[i][j][1]:
-                if table[i][j] >= table[i][j+1]+4:
-                    diff = (table[i][j] - table[i][j+1]) // 4
-                    tmp_table[i][j] -= diff
-                    tmp_table[i][j+1] += diff
-                elif table[i][j]+4 <= table[i][j+1]:
-                    diff = (table[i][j+1] - table[i][j]) // 4
-                    tmp_table[i][j+1] -= diff
-                    tmp_table[i][j] += diff
+def balance():
+    tmp_table = [[0] * (m + 1) for _ in range(n + 1)]
+    for r in range(1, n):
+        for c in range(1, m+1):
+            if blocks[r][c][3]:
+                continue
+            if table[r][c] > table[r+1][c]:
+                change_tmp_table(tmp_table, r, c, r+1, c)
+            else:
+                change_tmp_table(tmp_table, r+1, c, r, c)
 
-                # bigi, smalli, bigj, smallj = i, i, j, j+1
-                # if table[i][j] < table[i][j+1]:
-                #     bigj = j+1
-                #     smallj = j
-                # control_diff(tmp_table, bigi, smalli, bigj, smallj)
+    for r in range(1, n+1):
+        for c in range(1, m):
+            if blocks[r][c][0]:
+                continue
+            if table[r][c] > table[r][c+1]:
+                change_tmp_table(tmp_table, r, c, r, c+1)
+            else:
+                change_tmp_table(tmp_table, r, c+1, r, c)
 
-    for i in range(1, r):
-        for j in range(1, c+1):
-            if not wall[i+1][j][0]:
-                if table[i][j] >= table[i+1][j]+4:
-                    diff = (table[i][j] - table[i+1][j]) // 4
-                    tmp_table[i][j] -= diff
-                    tmp_table[i+1][j] += diff
-                elif table[i][j]+4 <= table[i+1][j]:
-                    diff = (table[i+1][j] - table[i][j]) // 4
-                    tmp_table[i+1][j] -= diff
-                    tmp_table[i][j] += diff
-
-                # bigi, smalli, bigj, smallj = i, i+1, j, j
-                # if table[i][j] < table[i+1][j]:
-                #     bigi = i+1
-                #     smalli = i
-                # control_diff(tmp_table, bigi, smalli, bigj, smallj)
-    for i in range(1, r+1):
-        for j in range(1, c+1):
-            table[i][j] += tmp_table[i][j]
+    for r in range(1, n+1):
+        for c in range(1, m+1):
+            table[r][c] += tmp_table[r][c]
 
 
-def cold_outside():
-    for i in range(1, r+1):
-        table[i][1] = max(0, table[i][1]-1)
-        table[i][c] = max(0, table[i][c]-1)
-    for j in range(2, c):
-        table[1][j] = max(0, table[1][j]-1)
-        table[r][j] = max(0, table[r][j]-1)
+def minus(r, c):
+    if table[r][c]:
+        table[r][c] -= 1
 
 
-def check():
-    for cr, cc in checks:
-        if table[cr][cc] < k:
+def minus_edges():
+    for c in range(1, m+1):
+        minus(1, c)
+        minus(n, c)
+    for r in range(2, n):
+        minus(r, 1)
+        minus(r, m)
+
+
+def check_all():
+    for r, c in check_list:
+        if table[r][c] < k:
             return False
     return True
 
 
-r, c, k = map(int, input().split())
-machines = []
-checks = []
+while True:
+    # 1. 바람 나옴
+    wind()
+    # 2. 온도가 조절됨
+    balance()
 
-for i in range(1, r+1):
-    lst = [0]
-    lst.extend(list(map(int, input().split())))
-    for j in range(1, c+1):
-        value = lst[j]
-        if value == 5:
-            checks.append((i, j))
-        elif value == 0:
-            pass
-        else:
-            machines.append((i, j, value))
-table = [[0]*(c+1) for _ in range(r+1)]
-w = int(input())
-wall = [[[False, False] for _ in range(c+1)] for _ in range(r+1)]
+    # 3. 가장 바깥쪽 온도 줄어듦
+    minus_edges()
+    # 4. 초콜릿 먹음
+    chocolate += 1
+    # 5. 모든 칸의 온도가 k 이상인지 검사
+    if check_all() or chocolate == 101:
+        break
 
-for _ in range(w):
-    x, y, t = map(int, input().split())
-    wall[x][y][t] = True
+
+print(chocolate)
 
 
 
-for i in range(100):
-    machine()
-    control()
-    cold_outside()
-    if check():
-       print(i+1)
-       exit()
 
-
-print(101)
 
